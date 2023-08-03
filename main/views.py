5cdfc2a08c84baf0f02da647b9c5836ae2983fdb
Wilyanse <willjose51@gmail.com>
2023-08-03 09:27:53 +0800

    Add landing page boilerplate
    Landing page includes non-functional link to login, signup, and about.

--- main/views.py
+++ main/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def LandingPage(response):
-    return HttpResponse("landing page")
+    context = {}
+    return render(response, "main/landingpage.html", context)
