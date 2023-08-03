8a57e7590d1c04e32c3126b80283074a4a98e099
Wilyanse <willjose51@gmail.com>
2023-08-03 09:43:56 +0800

    Change 'auth' to 'registration' app
    Auth had conflicting names with existing Django modules so it had to be renamed.

--- main/views.py
+++ main/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def LandingPage(response):
     context = {}
-    return render(response, "main/landingpage.html", context)
+    return render(response, "landing_page.html", context)
+
+def About(response):
+    context = {}
+    return render(response, "about.html", context)
+
+def Login(response):
+    pass
