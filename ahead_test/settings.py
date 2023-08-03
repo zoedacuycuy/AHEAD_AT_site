7195236debcbcd095a747b5a3603b1e58ffb726c
Wilyanse <willjose51@gmail.com>
2023-08-03 09:55:00 +0800

    Add login and register boilerplates
    Added links between the pages of different apps.

--- ahead_test/settings.py
+++ ahead_test/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'registration.apps.RegistrationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
